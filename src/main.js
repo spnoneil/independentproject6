f39ad994efbbeea7d5cf3b7b98cabca0187f196e
Scott O'Neil <scottieoneil@gmail.com>
2021-02-12 10:10:22 -0800

    get correct api call

--- src/main.js
+++ src/main.js
@@ -2,3 +2,30 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyService from './services/currency-service.js'
+
+// function clearForm() {
+//   $("")
+// }
+
+function getCurrency(response) { //MATH GO HERE?
+  if (response.result) {
+    console.log(response) // gets full object!
+    $('#output').text(`${response.conversion_rates}`)
+  } else {
+    $('#output').text(`${response}`)
+  }
+}
+
+async function apiCall(baseCurrency) {
+  const response = await CurrencyService.getCurrency(baseCurrency);
+  console.log(response)
+  getCurrency(response);
+}
+
+$(document).ready(function() {
+  $("#submitBtn").click(function() {
+    let baseCurrency = $("#baseCurrency").val();
+    apiCall(baseCurrency);
+  });
+})
--- src/services/currency-service.js
+++ src/services/currency-service.js
@@ -1,14 +1,13 @@
 export default class CurrencyService {
-  static getCurrency(baseCurrency) {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${baseCurrency}`)
-      .then(function(response) {
-        if (!response.ok) {
-          throw Error(response.statusText);
-        }
-        return response.json();
-      })
-      .catch(function(error) {
-        return error;
-      })
+  static async getCurrency(baseCurrency) {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${baseCurrency}`)
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch (error) {
+      return error;
+    }
   }
 }

e68d986763e5843d97e87d98d55c707b0aaf49ab
Scott O'Neil <scottieoneil@gmail.com>
2021-02-12 11:51:32 -0800

    attempt to work out more detailed error handling

--- src/services/currency-service.js
+++ src/services/currency-service.js
@@ -1,13 +1,18 @@
 export default class CurrencyService {
   static async getCurrency(baseCurrency) {
-    try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${baseCurrency}`);
-      if (!response.ok) {
-        throw Error(response.statusText);
+      try {
+        const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${baseCurrency}`)
+
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      } catch (error) {
+        return error;
       }
-      return response.json();
-    } catch (error) {
-      return error;
     }
-  }
+    // static async checkErrors(response)
+    // if (!response.ok) {
+    //   throw Error(respo)
+    // }
 }
